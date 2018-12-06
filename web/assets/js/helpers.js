b0a3410217476b6d419a1984f80468b69cf39e7c
Josef Renner <josef@josef.home>
2018-12-06 14:12:57 +0100

    fix typo in js

--- web/assets/js/helpers.js
+++ web/assets/js/helpers.js
@@ -53,9 +53,8 @@ hamburger.addEventListener('click', e => {
   navOver.classList.toggle('visible');
   main.classList.toggle('noScroll');
   footer.classList.toggle('noScroll');
-  header.classList.toggle('block');
   if (navOver.classList.contains('visible')) {
-    indow.scrollTo(0, 0);
+    window.scrollTo(0, 0);
   }
   // if (!shrinkHead) { header.classList.toggle('shrink'); } // only toggle shrinked header if still expanded
 }); 
