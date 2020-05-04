2c46bcd5720f2241295a5ab85fa4709186ea481d
Josef Renner <josef.renner@gmail.com>
2020-05-04 11:05:36 +0200

    add video button

--- web/assets/js/moreVideos.js
+++ web/assets/js/moreVideos.js
@@ -1,4 +1,3 @@
-
 function showVideos() {
   // a rather shameful way of implementing "load more" but there is
   // neither time nor money to do this the way it should be done..
@@ -20,9 +19,33 @@ function showVideos() {
   count += 6 // up number of visible videos
 }
 
-const videoButton = document.querySelector('#video-button');
+function hideVideos() {
+  // now it gets real lousy, i.e. redundant
+  // because it was not right again, and i really have no time
+  // and won’t be paid for extra stuff
+  let counter = 0
+  let count = 6
+  videos.forEach((video) => {
+    if (counter < count) {
+      video.style.display = "block"
+      console.log("show")
+    } else {
+      video.style.display = "none"
+      console.log("hide")
+    }
+    counter += 1
+  })
+  if (count > videos.length) {
+    // hide button when no more videos left
+    videoButton.style.display = "none"
+  }
+}
+
+const showVideoButton = document.querySelector('#video-button');
+const hideVideoButton = document.querySelector('#hide-video-button');
 const videos = document.querySelectorAll('.video-item'); 
 let count = 6
 showVideos()
 
-videoButton.addEventListener('click' || 'touch', showVideos, false)
+showVideoButton.addEventListener('click' || 'touch', showVideos, false)
+hideVideoButton.addEventListener('click' || 'touch', hideVideos, false)
