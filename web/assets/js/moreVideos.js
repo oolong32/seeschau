4849c67431bf8dba301e6b63852c41ba34d2cb8e
Josef Renner <josef.renner@gmail.com>
2020-05-04 11:30:30 +0200

    fix less videos button

--- web/assets/js/moreVideos.js
+++ web/assets/js/moreVideos.js
@@ -1,7 +1,8 @@
 function showVideos() {
   // a rather shameful way of implementing "load more" but there is
   // neither time nor money to do this the way it should be done..
-  let counter = 0
+  counter = 0
+  let count = 10 // shadow count
   videos.forEach((video) => {
     if (counter < count) {
       video.style.display = "block"
@@ -12,19 +13,15 @@ function showVideos() {
     }
     counter += 1
   })
-  if (count > videos.length) {
-    // hide button when no more videos left
-    videoButton.style.display = "none"
-  }
-  count += 6 // up number of visible videos
+  showVideoButton.style.display = "none"
+  hideVideoButton.style.display = "block";
 }
 
 function hideVideos() {
   // now it gets real lousy, i.e. redundant
   // because it was not right again, and i really have no time
   // and won’t be paid for extra stuff
-  let counter = 0
-  let count = 6
+  counter = 0
   videos.forEach((video) => {
     if (counter < count) {
       video.style.display = "block"
@@ -35,17 +32,29 @@ function hideVideos() {
     }
     counter += 1
   })
-  if (count > videos.length) {
-    // hide button when no more videos left
-    videoButton.style.display = "none"
-  }
+  showVideoButton.style.display = "block"
+  hideVideoButton.style.display = "none";
 }
 
 const showVideoButton = document.querySelector('#video-button');
 const hideVideoButton = document.querySelector('#hide-video-button');
 const videos = document.querySelectorAll('.video-item'); 
 let count = 6
-showVideos()
+let counter = 0
+
+// initialise this stuff
+videos.forEach(video => {
+  if (counter < count) {
+    video.style.display = "block"
+    console.log("show")
+  } else {
+    video.style.display = "none"
+    console.log("hide")
+  }
+  counter += 1
+})
+showVideoButton.style.display = "block"
+hideVideoButton.style.display = "none";
 
 showVideoButton.addEventListener('click' || 'touch', showVideos, false)
 hideVideoButton.addEventListener('click' || 'touch', hideVideos, false)
